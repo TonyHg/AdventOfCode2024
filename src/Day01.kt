1c36143ad7fc789f0fdcbaffa286b6ebc3791a1b
Tony Heng <tony.heng@doctolib.com>
2024-12-03 19:49:44 +0100

    feat: add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,21 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun sumOfSmallestDistance(list1: MutableList<Int>, list2: MutableList<Int>): Int {
+        list1.sort()
+        list2.sort()
+        return list1.zip(list2).fold(0) { acc, (n1, n2) -> acc + abs(n1 - n2) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+
+    input.forEach { line ->
+        val (n1, n2) = line.split("   ").map { it.toInt() }
+        list1.add(n1)
+        list2.add(n2)
+    }
+
+    sumOfSmallestDistance(list1, list2).println()
 }
