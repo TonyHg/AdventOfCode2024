df9f7326dc118de71b349991c8a6b4d42a2aec4d
Tony Heng <tony.heng@doctolib.com>
2024-12-03 21:08:48 +0100

    feat: add Day02 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,22 +1,40 @@
 import kotlin.math.abs
 
 fun main() {
+    fun isStepValid(increasing: Boolean, a: Int, b: Int): Boolean {
+        return (increasing && a < b || !increasing && a > b) && abs(a - b) <= 3
+    }
+
     fun isLevelSafe(level: List<Int>): Boolean {
         if (level.size < 2) return true
         val increasing = level[0] - level[1] < 0
 
-        return level.zipWithNext().all { (a, b) ->
-            val diff = abs(a - b)
-            (increasing && a < b || !increasing && a > b) && diff <= 3
+        return level.zipWithNext().all { (a, b) -> isStepValid(increasing, a, b) }
+    }
+
+    fun isLevelSafeWithTolerance(level: List<Int>): Boolean {
+        if (isLevelSafe(level)) return true
+
+        for (i in level.indices) {
+            val mutableList = level.toMutableList()
+            mutableList.removeAt(i)
+            if (isLevelSafe(mutableList)) return true
         }
+
+        return false
     }
 
     fun nbSafeLevels(levels: List<List<Int>>): Int {
         return levels.count { isLevelSafe(it) }
     }
 
+    fun nbSafeTolerantLevels(levels: List<List<Int>>): Int {
+        return levels.count { isLevelSafeWithTolerance(it) }
+    }
+
     val input = readInput("Day02")
     val levels = input.map { line -> line.split(" ").map { it.toInt() } }
 
     println("nbSafeLevels ${nbSafeLevels(levels)}")
+    println("nbSafeTolerantLevels ${nbSafeTolerantLevels(levels)}")
 }
