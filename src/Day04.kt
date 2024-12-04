d8d405e68a835d8ad3c18889fd758b21c7488ed4
Tony Heng <tony.heng@doctolib.com>
2024-12-04 21:07:23 +0100

    feat: add Day04 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,5 +1,3 @@
-import kotlin.math.abs
-
 fun main() {
     fun checkTopLeft(matrix: List<List<Char>>, i: Int, j: Int): Int {
         return if (i - 3 >= 0 && j - 3 >= 0 && matrix[i][j] == 'X' && matrix[i - 1][j - 1] == 'M'
@@ -63,7 +61,26 @@ fun main() {
         return count
     }
 
+    fun checkCross(matrix: List<List<Char>>, i: Int, j: Int): Boolean {
+        return ((matrix[i - 1][j - 1] == 'M' && matrix[i + 1][j + 1] == 'S') || (matrix[i - 1][j - 1] == 'S' && matrix[i + 1][j + 1] == 'M')) &&
+                ((matrix[i - 1][j + 1] == 'M' && matrix[i + 1][j - 1] == 'S') || (matrix[i - 1][j + 1] == 'S' && matrix[i + 1][j - 1] == 'M'))
+    }
+
+    fun countXMAS2(matrix: List<List<Char>>): Int {
+        var count = 0
+
+        for (i in 1..matrix.size - 2) {
+            for (j in 1..matrix[i].size - 2) {
+                if (matrix[i][j] == 'A' && checkCross(matrix, i, j))
+                    count++
+            }
+        }
+
+        return count
+    }
+
     val input = readInput("Day04")
     val matrix = input.map { line -> line.toList() }
     println("countXMAS ${countXMAS(matrix)}")
+    println("countXMAS2 ${countXMAS2(matrix)}")
 }
