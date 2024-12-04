80b95f4cc97021eeb0a7e0ee534105a8b322ab49
Tony Heng <tony.heng@doctolib.com>
2024-12-04 20:30:24 +0100

    feat: add Day03

---
+++ src/Day03.kt
@@ -0,0 +1,19 @@
+import kotlin.math.abs
+
+fun main() {
+    fun regexMul(line: String): Int {
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        return regex.findAll(line).sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+    }
+
+    fun removeBetweenDontAndDo(input: String): String {
+        val regex = Regex("don't\\(\\).*?(do\\(\\)|$)")
+        return input.replace(regex, "")
+    }
+
+    val input = readInput("Day03")
+    val result = input.sumOf { regexMul(it) }
+    println("regexMul $result")
+    val result2 = input.sumOf { regexMul(removeBetweenDontAndDo(it)) }
+    println("removeBetweenDontAndDo $result2")
+}
