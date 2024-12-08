1635e0f1ffb29a3a78385fc4052b0f9326cc5682
Tony Heng <tony.heng@doctolib.com>
2024-12-08 13:36:10 +0100

    feat: add Day08 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -2,8 +2,10 @@ typealias Nodes = Map<Char, List<Position>>
 
 fun main() {
     fun getAntiNode(node: Position, other: Position): List<Position> {
-        return listOf(Pair(2 * node.first - other.first, 2 * node.second - other.second),
-            Pair(2 * other.first - node.first, 2 * other.second - node.second))
+        return listOf(
+            Pair(2 * node.first - other.first, 2 * node.second - other.second),
+            Pair(2 * other.first - node.first, 2 * other.second - node.second)
+        )
     }
 
     fun computeAntiNodes(nodes: Nodes, height: Int, width: Int): Int {
@@ -15,6 +17,37 @@ fun main() {
                     .forEach { antiNodes.add(it) }
             }
         }
+        return antiNodes.size
+    }
+
+    fun getMoreAntiNodes(node: Position, other: Position): List<Position> {
+        val distY = node.first - other.first
+        val distX = node.second - other.second
+
+        val antiNodes = mutableListOf<Position>()
+        // TODO: improve by only adding when the position is in the grid
+        for (i in 1..100) {
+            antiNodes.add(Pair(node.first + i * distY, node.second + i * distX))
+            antiNodes.add(Pair(other.first - i * distY, other.second - i * distX))
+        }
+        return antiNodes
+    }
+
+    fun moreAntiNodes(nodes: Nodes, height: Int, width: Int): Int {
+        val antiNodes = mutableSetOf<Position>()
+
+        nodes.forEach { (_, value) ->
+            value.forEachIndexed { index, curr ->
+                value.drop(index + 1).flatMap { getMoreAntiNodes(curr, it) }
+                    .filter { it.first in 0..<height && it.second in 0..<width }
+                    .forEach { antiNodes.add(it) }
+            }
+
+            if (value.size > 1) {
+                value.forEach { antiNodes.add(it) }
+            }
+        }
+
         return antiNodes.size
     }
 
@@ -34,4 +67,5 @@ fun main() {
     val input = readInput("Day08")
     val parsedInput = parseInput(input)
     println("computeAntiNodes ${computeAntiNodes(parsedInput, input.size, input[0].length)}")
+    println("moreAntiNodes ${moreAntiNodes(parsedInput, input.size, input[0].length)}")
 }
